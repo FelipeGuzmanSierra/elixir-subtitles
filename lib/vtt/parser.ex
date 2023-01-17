8aff079407d43a3b7f7add413ff39765702e3ae9
FelipeGuzmanSierra <97761783+FelipeGuzmanSierra@users.noreply.github.com>
2023-01-17 08:58:22 -0500

    Update dependencies and add a dot case to the vtt regex

--- lib/vtt/parser.ex
+++ lib/vtt/parser.ex
@@ -1,9 +1,10 @@
 defmodule Subtitles.Vtt.Parser do
-  @time_pattern ~r/\d{2}:\d{2}:\d{2}\.\d{3}/
+  @time_pattern ~r/\d{2}:\d{2}:\d{2}[\,, \.]\d{3}/
   @unsupported_blocks ["WEBVTT", "NOTE", "REGION", "STYLE"]
 
   def parse(subtitle) do
-    subs = subtitle
+    subs =
+      subtitle
       |> Utils.split_lines()
       |> filter_unsupported_blocks()
       |> parse_cues()
@@ -17,7 +18,8 @@ defmodule Subtitles.Vtt.Parser do
   end
 
   defp parse_cues(blocks) do
-    blocks |> Enum.map(fn block ->
+    blocks
+    |> Enum.map(fn block ->
       block
       |> String.split("\n")
       |> remove_cue_headers()
@@ -25,12 +27,12 @@ defmodule Subtitles.Vtt.Parser do
     end)
   end
 
-  defp remove_cue_headers(cue = [first|rest]) do
-    @time_pattern |> Regex.match?(first) && cue || rest
+  defp remove_cue_headers(cue = [first | rest]) do
+    (@time_pattern |> Regex.match?(first) && cue) || rest
   end
 
-  defp make_subtitle([timings|parts]) do
-    [[from], [to]|_] = @time_pattern |> Regex.scan(timings)
+  defp make_subtitle([timings | parts]) do
+    [[from], [to] | _] = @time_pattern |> Regex.scan(timings)
 
     Cue.new(from, to, parts |> remove_timestamps())
   end
